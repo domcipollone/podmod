2640e875bd5fec49b3f4a220f84eed82deea18e1
domcipollone <dchip95@gmail.com>
2025-07-28 09:12:34 -0400

    fix joblib load

--- model_pipeline.py
+++ model_pipeline.py
@@ -19,8 +19,7 @@ st.set_page_config(
 @st.cache_resource
 def load_model():
     try:
-        with open('feature_analysis/models/xgb_best_model_20250727.joblib', 'rb') as f:
-            return load(f)
+        load('feature_analysis/models/xgb_best_model_20250727.joblib')
     except:
         st.error("Model file not found! Please upload your model joblib")
         return None
